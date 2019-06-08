db6999d42a867d4e9e9b155629ace6a844c813e0
Pawel Lorek <palorek@gmail.com>
2019-06-08 13:52:42 +0200

    Add test navigation script & generated maps
    with gmapping from last step's SLAM.

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+if [[ `pwd` == *"scripts" ]]; then
+  cd ..
+fi
+echo $(pwd)
+
+xterm  -e  "source devel/setup.zsh; roscore" &
+
+sleep 4
+xterm  -e  "source devel/setup.zsh; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/my_robot/worlds/world4.world" &
+
+
+sleep 5
+xterm  -e  "source devel/setup.zsh; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/maps/world43.yaml" &
+
+sleep 5
+xterm  -e  " source devel/setup.zsh; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
